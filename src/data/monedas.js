8e02a2b4fa04a20d9ad7f88a290317618cf27e88
Franco Coll <francoadrian.coll2012@gmail.com>
2022-05-07 11:55:42 -0300

    first commit

---
+++ src/data/monedas.js
@@ -0,0 +1,10 @@
+const monedas = [
+    {id:'USD', nombre:'Dolar de EEUU'},
+    {id:'MXN', nombre:'Peso mexicano'},
+    {id:'EUR', nombre:'Euro'},
+    {id:'GBP', nombre:'Libre esterlina'},
+  ]
+
+  export {
+      monedas
+  }
